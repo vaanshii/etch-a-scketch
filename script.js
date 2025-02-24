3fcd793e73695f1e06f890e68ed45bc2d5f03e8b
Ivan Russel <russelblanquiza@gmail.com>
2025-02-24 19:07:07 +0800

    Fixed proper naming convension. Declared constant variable for elements in js.

---
+++ script.js
@@ -0,0 +1,6 @@
+const screenContainer = document.querySelector(".sketch-screen-container");
+const rainbowToggle = document.querySelector("#rainbow-toggle");
+const pixelSlider = document.querySelector(".pixel-slider");
+const clearButton = document.querySelector("#clear-button");
+const colorPicker = document.querySelector("#color-picker");
+const eraserButton = document.querySelector("#eraser-button");
